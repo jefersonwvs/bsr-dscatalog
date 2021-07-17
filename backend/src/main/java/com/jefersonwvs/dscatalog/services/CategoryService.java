6d5dc36e4614192dea62bf20115de8a9bf729929
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-07-17 10:52:22 -0400

    Class CategoryDTO created

--- backend/src/main/java/com/jefersonwvs/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jefersonwvs/dscatalog/services/CategoryService.java
@@ -1,23 +1,29 @@
 package com.jefersonwvs.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jefersonwvs.dscatalog.dto.CategoryDTO;
 import com.jefersonwvs.dscatalog.entities.Category;
 import com.jefersonwvs.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list
+				.stream()
+				.map(x -> new CategoryDTO(x))
+				.collect(Collectors.toList());
 	}
 
 }

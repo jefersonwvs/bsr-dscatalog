a631dd5d8e21eeaef887d1a4c308a5af2bf9b30d
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-07-18 11:56:05 -0400

    Endpoint for deleting Category created

--- backend/src/main/java/com/jefersonwvs/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/jefersonwvs/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,24 +9,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.jefersonwvs.dscatalog.services.exceptions.DatabaseException;
 import com.jefersonwvs.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e,
-			HttpServletRequest request) {
-		
+	public ResponseEntity<StandardError> entityNotFound(
+			ResourceNotFoundException e,
+			HttpServletRequest request) 
+	{
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError error = new StandardError();
+		
 		error.setTimestamp(Instant.now());
-		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setStatus(status.value());
 		error.setError("Recurso não encontrado!");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-			
+		return ResponseEntity.status(status).body(error);
 	}
-
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(
+			DatabaseException e,
+			HttpServletRequest request) 
+	{
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError();
+		
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Erro de banco de dados!");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+	
 }
--- backend/src/main/java/com/jefersonwvs/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jefersonwvs/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jefersonwvs.dscatalog.dto.CategoryDTO;
 import com.jefersonwvs.dscatalog.entities.Category;
 import com.jefersonwvs.dscatalog.repositories.CategoryRepository;
+import com.jefersonwvs.dscatalog.services.exceptions.DatabaseException;
 import com.jefersonwvs.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,17 @@ public class CategoryService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade!");
+		}
+		
 	}
 	
 }

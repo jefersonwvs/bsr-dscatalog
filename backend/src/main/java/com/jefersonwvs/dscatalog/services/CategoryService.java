01ba5459cb1ccb6b46ba5378cb9ff02baed0ff4c
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-07-18 11:18:43 -0400

    Endpoint for updating Category created

--- backend/src/main/java/com/jefersonwvs/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jefersonwvs/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jefersonwvs.dscatalog.dto.CategoryDTO;
 import com.jefersonwvs.dscatalog.entities.Category;
 import com.jefersonwvs.dscatalog.repositories.CategoryRepository;
-import com.jefersonwvs.dscatalog.services.exceptions.EntityNotFoundException;
+import com.jefersonwvs.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,7 +30,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada!"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada!"));
 		return new CategoryDTO(entity);
 	}
 
@@ -39,7 +41,17 @@ public class CategoryService {
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
-	
-	
 
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
+		}
+	}
+	
 }

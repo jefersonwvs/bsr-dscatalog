109898bba030d3a9cf801c4e3bd19c41364bf22c
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-08-01 10:56:35 -0400

    User CRUD implemented

--- backend/src/main/java/com/jefersonwvs/dscatalog/entities/User.java
+++ backend/src/main/java/com/jefersonwvs/dscatalog/entities/User.java
@@ -5,7 +5,9 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,10 +27,11 @@ public class User implements Serializable {
 	private Long id;
 	private String firstName;
 	private String lastName;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER) /* Carrega os roles junto com as demais informações do User*/
 	@JoinTable(name = "tb_user_role", 
 				  joinColumns = @JoinColumn(name = "user_id"),
 				  inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -49,37 +52,47 @@ public class User implements Serializable {
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	public Set<Role> getRoles() {
 		return roles;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
---
+++ backend/src/main/java/com/jefersonwvs/dscatalog/services/UserService.java
@@ -0,0 +1,94 @@
+package com.jefersonwvs.dscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jefersonwvs.dscatalog.dto.RoleDTO;
+import com.jefersonwvs.dscatalog.dto.UserDTO;
+import com.jefersonwvs.dscatalog.dto.UserInsertDTO;
+import com.jefersonwvs.dscatalog.entities.Role;
+import com.jefersonwvs.dscatalog.entities.User;
+import com.jefersonwvs.dscatalog.repositories.RoleRepository;
+import com.jefersonwvs.dscatalog.repositories.UserRepository;
+import com.jefersonwvs.dscatalog.services.exceptions.DatabaseException;
+import com.jefersonwvs.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable) {
+		Page<User> list = repository.findAll(pageable);
+		return list.map(entity -> new UserDTO(entity));
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada!"));
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado!");
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade!");
+		}
+	}
+	
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		
+		entity.getRoles().clear();
+		for (RoleDTO obj : dto.getRoles()) {
+			Role role = roleRepository.getOne(obj.getId());
+			entity.getRoles().add(role);
+		}
+	}
+	
+}

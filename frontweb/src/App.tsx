64b4b92b66b87b0e0653b4e5ef522668bf224f2d
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-10-02 19:42:08 -0400

    Bootstrap theme customization

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,7 +1,10 @@
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
-   return <h1>Hello, world!</h1>;
+   return ( 
+      <h1>Hello, world!</h1>
+   );
 }
 
 export default App;

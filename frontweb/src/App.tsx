b5b7377d70b6fe5859ddd64cc38540c1102555a2
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-10-03 11:56:23 -0400

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 const App = function () {
    return (
       <>
-         <Navbar />
-         <h1>Hello, world!</h1>
+         <Home />
       </>
    );
 };

021071053fad34f053e5437992d75e27703cb23b
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-10-02 20:28:18 -0400

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,14 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
-function App() {
-   return ( 
-      <h1>Hello, world!</h1>
+const App = function () {
+   return (
+      <>
+         <Navbar />
+         <h1>Hello, world!</h1>
+      </>
    );
-}
+};
 
 export default App;

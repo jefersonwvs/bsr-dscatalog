4b66724e6b2ce814ece8e0ec64e51e590121c748
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-10-12 15:37:26 -0400

    Props ProductCard

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -1,17 +1,21 @@
 import './index.css';
 
-import ProductImg from 'assets/images/product.png';
 import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
 
-const ProductCard = () => {
+type Props = {
+   product: Product;
+};
+
+const ProductCard = ({ product }: Props) => {
    return (
       <div className="base-card product-card">
          <div className="card-top-container">
-            <img src={ProductImg} alt="img-computer" />
+            <img src={product.imgUrl} alt={product.name} />
          </div>
          <div className="card-bottom-container">
-            <h6>Computador</h6>
-            <ProductPrice />
+            <h6>{product.name}</h6>
+            <ProductPrice price={product.price} />
          </div>
       </div>
    );

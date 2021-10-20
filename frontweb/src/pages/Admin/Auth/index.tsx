89206bc8710b24cf311db21ec0f6027932efc120
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2021-10-20 10:50:38 -0400

    Banner styling

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,10 +1,11 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Switch, Route } from 'react-router';
+import './styles.css';
 
 const Auth = () => {
    return (
       <div className="auth-container">
-         <div className="auth-banner">
+         <div className="auth-banner-container">
             <h1>Divulgue seus produtos no DS Catalog</h1>
             <p>
                Faça parte do nosso catálogo de divulgação e aumente a venda dos

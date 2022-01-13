a2975310a0aba33bb5f3a5c3f75dc2402063f954
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-01-13 11:17:44 -0400

    Layout x-mall finished for Navbar and Home

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,16 +1,9 @@
-import Navbar from 'components/Navbar';
-
+import Home from 'pages/Home';
 import './assets/styles/custom.scss'; // bootstrap theme
 import './App.css'; // global styles
 
 function App() {
-  return (
-    // React components are written in JSX - it's sort of HTML mixed with JS
-    <>
-      <Navbar />
-      <h1>Hello, DSCatalog</h1>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import Navbar from 'components/Navbar';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
+
+import './styles.css';
+
+const Home = function () {
+  return (
+    // React components are written in JSX - it's sort of HTML mixed with JS
+    <>
+      <Navbar />
+
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado.
+            </p>
+            <ButtonIcon />
+          </div>
+
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;

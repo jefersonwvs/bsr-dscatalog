cfd381f335b6119a8c4fc270920b7f176e983fae
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-01-17 15:37:37 -0400

    Catalog - ProductDetails links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 
@@ -7,10 +9,10 @@ const ProductDetails = function () {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
+        <Link to="/products" className="goback-container">
           <ArrowIcon />
           <h2>Voltar</h2>
-        </div>
+        </Link>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">

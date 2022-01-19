ebf3a1e318b23b9cd447f649aba17e3261e57cba
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-01-19 13:44:51 -0400

    Auth routes

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -5,25 +5,37 @@ import Home from 'pages/Home';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
 import ProductDetails from 'pages/ProductDetails';
+import Auth from 'pages/Admin/Auth';
 
+// prettier-ignore
 const Routes = function () {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
+        
         <Route path="/" exact>
           <Home />
         </Route>
+
         <Route path="/products" exact>
           <Catalog />
         </Route>
+        
         <Route path="/products/:productId">
           <ProductDetails />
         </Route>
+        
+        <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+        <Route path="/admin/auth">
+          <Auth />
+        </Route>
+        
         <Redirect from="/admin" to="/admin/products" exact />
         <Route path="/admin">
           <Admin />
         </Route>
+      
       </Switch>
     </BrowserRouter>
   );

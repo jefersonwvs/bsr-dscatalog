479f988c6ff3edd2d101b8a1631b8430b20d4ff9
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-02-10 15:25:09 -0400

    Testing another hasAnyRoles scenario

--- frontweb/src/utils/__tests__/auth.spec.ts
+++ frontweb/src/utils/__tests__/auth.spec.ts
@@ -21,4 +21,16 @@ describe('hasAnyRoles tests', () => {
     const result = hasAnyRoles(roles);
     expect(result).toEqual(true);
   });
+
+  test('should return false when user does not have given role', () => {
+    jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+      exp: 0,
+      user_name: '',
+      authorities: ['ROLE_OPERATOR'],
+    });
+    const roles: Role[] = ['ROLE_ADMIN'];
+
+    const result = hasAnyRoles(roles);
+    expect(result).toEqual(false);
+  });
 });

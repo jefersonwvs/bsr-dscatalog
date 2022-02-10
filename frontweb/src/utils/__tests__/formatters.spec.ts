1f06bd7238b562f92e4ee8b001d7650a392a68a8
Jeferson Willian Vieira Silva <jefersonwillians137@gmail.com>
2022-02-10 09:50:45 -0400

    Using describe for correlated tests

--- frontweb/src/utils/__tests__/formatters.spec.ts
+++ frontweb/src/utils/__tests__/formatters.spec.ts
@@ -1,12 +1,34 @@
 import { formatPrice } from 'utils/formatters';
 
-test('formatPrice should format number pt-BR when given 10.1', () => {
-  // Arrange
-  const value = 10.1;
+describe('formatPrice for positive numbers', function () {
+  test('formatPrice should format number pt-BR when given 10.1', () => {
+    // Arrange
+    const value = 10.1;
 
-  // Act
-  const result = formatPrice(value);
+    // Act
+    const result = formatPrice(value);
 
-  // Assert
-  expect(result).toEqual('10,10');
+    // Assert
+    expect(result).toEqual('10,10');
+  });
+
+  test('formatPrice should format number pt-BR when given 0.1', () => {
+    const value = 0.1;
+    const result = formatPrice(value);
+    expect(result).toEqual('0,10');
+  });
+});
+
+describe('formatPrice for non-positive numbers', function () {
+  test('formatPrice should format number pt-BR when given 0', () => {
+    const value = 0;
+    const result = formatPrice(value);
+    expect(result).toEqual('0,00');
+  });
+
+  test('formatPrice should format number pt-BR when given -5.3', () => {
+    const value = -5.3;
+    const result = formatPrice(value);
+    expect(result).toEqual('-5,30');
+  });
 });
